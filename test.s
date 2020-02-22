77157039d1c832406d8ec5ec2a65b8cde967adb1
HumveeA6 <humveea6@163.com>
2020-02-22 16:29:50 +0800

    注释说明

---
+++ test.s
@@ -0,0 +1,31 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movq	__ZNSt3__13cinE@GOTPCREL(%rip), %rdi
+	leaq	-8(%rbp), %rsi
+	callq	__ZNSt3__113basic_istreamIcNS_11char_traitsIcEEErsERi
+	leaq	-4(%rbp), %rsi
+	movq	%rax, %rdi
+	callq	__ZNSt3__113basic_istreamIcNS_11char_traitsIcEEErsERi
+	movl	-4(%rbp), %esi
+	addl	-8(%rbp), %esi
+	movq	__ZNSt3__14coutE@GOTPCREL(%rip), %rdi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEi
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+
+.subsections_via_symbols
